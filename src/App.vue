aae6195b0e62a42e2c7a865ea8872a9a878454cd
waynenguyen1302 <quang.ndm91@gmail.com>
2023-07-30 16:10:28 -0700

    modified App.Vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script setup>
+<script>
 
 </script>
 
 <template>
+  <div id="app">
 
+    <router-view/>
+
+  </div>
 </template>
 
 <style>
+:root {
+  --orange: #E88607;
+  --lightorange: #ff9c4a;
+  --lightgray: #a0a0a0;
+  --darkgray: #181818;
+  --green: #2cce7d;
+  --red: #fa3d3d;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Monserat', sans-serif;
+  list-style: none;
+
+  
+}
+a {
+    text-decoration: none;
+    color: black;
+}
+
+body {
+  background-color: #fff;
+}
 
 </style>
+
